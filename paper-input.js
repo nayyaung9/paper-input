b86dfb8a3b6f3fa0a769c54eb1f0b8aad7f8cb4f
nayyaung9 <naygit.developer@gmail.com>
2019-10-24 10:41:59 +0630

    add template element to shadow DOM

--- paper-input.js
+++ paper-input.js
@@ -1,7 +1,15 @@
+const template = document.createElement('template');
+template.innerHTML = `
+  <input type="text" name="paper-input" />
+`;
+
 class PaperInput extends HTMLElement {
   constructor() {
     super();
     this.innerHTML = 'Input Something'
+
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 
   connectedCallback() {
